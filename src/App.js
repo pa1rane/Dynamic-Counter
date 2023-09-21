f15fe0118fbc8bc1e7f46b6b25983fd9b826380f
Pavan Rane <ranepavanrp@gmail.com>
2023-09-21 23:20:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+ const [count, setCount] = useState(0);
+ const [inputValue, setInputValue] = useState(0)
+
+ const handleIncrement = () => {
+    setCount(inputValue > 1 ? count + parseInt(inputValue) : count + 1)
+    console.log(count)
+ }
+
+ const handleDecrement = () => {
+    setCount(inputValue > 1 ? count - parseInt(inputValue) : count - 1)
+ }
+
+ const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+    console.log(inputValue)
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <div className='buttons'>
+        <button className='decrement' onClick={handleDecrement}>-</button>
+        <span>  </span>
+        <button className='increment' onClick={handleIncrement}>+</button>
+      </div>
+      <div className='operate'>
+      <p>Increment OR Decrement by</p>
+      <input type="number" value={inputValue} onChange={handleInputChange}/>
+      </div>
+      <button className='reset'>Reset</button>
+    </>
+  )
 }
 
-export default App;
+export default App
